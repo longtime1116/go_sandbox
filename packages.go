f55a7da9208b02c61949114cf4b8a8bea60983a5
longtime1116 <longtime1116@gmail.com>
2018-12-30 01:40:14 +0900

    omit type of args

--- packages.go
+++ packages.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 )
 
+func add_omit_type(x, y int) int {
+	return x + y
+}
+
 func add(x int, y int) int {
 	return x + y
 }
 
 func main() {
-	fmt.Println("My favorite number is", add(2, 5))
+	fmt.Println("My favorite number is", add(2, 5)+add_omit_type(3, 4))
 }
