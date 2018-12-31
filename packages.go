3fc31d1e8bf3410e5ea52a34ba5f353a968bd127
longtime1116 <longtime1116@gmail.com>
2018-12-31 11:59:22 +0900

    Numeric Constants

--- packages.go
+++ packages.go
@@ -31,6 +31,20 @@ func SplitDate(s string) (y, m, d int) {
 	return
 }
 
+// Numeric Constants
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
+)
+
+func needInt(x int) int {
+	return x*10 + 1
+}
+
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
 func main() {
 	fmt.Println("Function.", add(2, 5)+AddOmitType(3, 4))
 
@@ -57,4 +71,14 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
 	fmt.Printf("Type: %T Value: %#U\n", str, str)
+
+	const Truth = true
+	// Truth = false  // エラー
+
+	// Numeric Constants は高精度の状態で値を保持できる。Int64 では収まりきらないレベルのものも保持できる
+	fmt.Println(needInt(Small))
+	// fmt.Println(needInt(Big)) // overflow
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+
 }
