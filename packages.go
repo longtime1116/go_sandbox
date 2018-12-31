59d34f21578c082c44ca6ed1cf3e0acc75b1a38b
longtime1116 <longtime1116@gmail.com>
2018-12-31 11:31:22 +0900

    variables

--- packages.go
+++ packages.go
@@ -6,6 +6,10 @@ import (
 	"strings"
 )
 
+var package_variable1 bool                                  // package 内で利用可能
+var package_variable2, package_variable3 bool = true, false // 初期化
+var package_variable4, package_variable5 = true, false      // 初期化子があれば型省略可能
+
 func add(x int, y int) int {
 	return x + y
 }
@@ -34,4 +38,11 @@ func main() {
 
 	year, month, day := SplitDate("1991-11-16")
 	fmt.Printf("Year: %d\nMonth: %d\nDay: %d\n", year, month, day)
+
+	var c, python, java = true, false, "No!"
+	fmt.Println("variables")
+	fmt.Println(c, python, java)
+	fmt.Println(package_variable1, package_variable2, package_variable3, package_variable4, package_variable5)
+
+	i := 0 // var 宣言を使わず暗黙的に型宣言
 }
