fd21f582d72fc4062ff4541b9c3d9bd1962e3059
shumo.tokinaga <longtime1116@gmail.com>
2020-07-22 16:23:26 +0900

    use channel as semaphore

--- effective_go/main.go
+++ effective_go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 type Sample struct {
@@ -62,31 +63,78 @@ func init() {
 	fmt.Println("init() called implicitly before main is called")
 }
 
+func Announce(m string) {
+	// 関数リテラルをうまく使うと良い
+	go func() {
+		time.Sleep(time.Second)
+		fmt.Println(m)
+	}()
+}
+
 func main() {
-	// ローカル変数のアドレスを戻り値にするのは完全に合法であり、変数領域は関数が帰った後も保持される
-	s := f()
-	fmt.Println(s)
+	if false {
+		// ローカル変数のアドレスを戻り値にするのは完全に合法であり、変数領域は関数が帰った後も保持される
+		s := f()
+		fmt.Println(s)
 
-	// print
-	// 10進数表記、16進数表記
-	var x int = 1<<4 + 1<<2
-	fmt.Printf("%d %x\n", x, x)
+		// print
+		// 10進数表記、16進数表記
+		var x int = 1<<4 + 1<<2
+		fmt.Printf("%d %x\n", x, x)
 
-	fmt.Printf("%T\n", s)
-	fmt.Printf("%v\n", s)
-	fmt.Printf("%+v\n", s)
-	fmt.Printf("%#v\n", s)
+		fmt.Printf("%T\n", s)
+		fmt.Printf("%v\n", s)
+		fmt.Printf("%+v\n", s)
+		fmt.Printf("%#v\n", s)
 
-	// 可変長引数
-	f2(1, "abc")
-	f2(1, 2, 3, 4, "abc")
+		// 可変長引数
+		f2(1, "abc")
+		f2(1, 2, 3, 4, "abc")
 
-	// 定数
-	const Cnst = "const"
-	const Pi = math.Pi
-	// 式が右辺にあると、実行時にメモリ確保できないので定数とはならない。エラー。
-	// const PiSqrt = math.Sqrt(math.Pi)
-	fmt.Println(Cnst, Pi)
-	fmt.Println(Red, Blue, Yellow)
-	fmt.Println(KB, MB, GB)
+		// 定数
+		const Cnst = "const"
+		const Pi = math.Pi
+		// 式が右辺にあると、実行時にメモリ確保できないので定数とはならない。エラー。
+		// const PiSqrt = math.Sqrt(math.Pi)
+		fmt.Println(Cnst, Pi)
+		fmt.Println(Red, Blue, Yellow)
+		fmt.Println(KB, MB, GB)
+
+		Announce("announced!")
+		time.Sleep(2000 * time.Millisecond)
+	}
+	{
+		// チャンネルを使った便利なイディオム！
+		c := make(chan int)
+		go func() {
+			// 時間のかかる処理
+			time.Sleep(time.Second)
+			// なんでもいいから値を入れる
+			c <- 1
+		}()
+		fmt.Println("waiting...")
+		// 値を受け取るまで待ってくれる
+		<-c
+		fmt.Println("done!")
+	}
+	{
+		// 第二引数でバッファする数を設定できるので、これをセマフォとして活用できる
+		semaphore := make(chan int, 5)
+		for i := 0; i < 10; i++ {
+			go func(i int) {
+				semaphore <- 1
+				fmt.Println("i:", i)
+			}(i)
+		}
+		fmt.Println("waiting...")
+		time.Sleep(time.Second * 2)
+		for i := 0; i < 5; i++ {
+			<-semaphore
+		}
+		time.Sleep(time.Second)
+		// このforでchannelから値を取り出して初めてiのPrintlnが実行される
+		for i := 0; i < 5; i++ {
+			<-semaphore
+		}
+	}
 }
