9fa7b720813b37ec7e0653bb4b6df5623d98763d
shumo.tokinaga <longtime1116@gmail.com>
2020-08-02 01:07:16 +0900

    start chapter3

---
+++ system_programming/chapter3/exec.go
@@ -0,0 +1,9 @@
+package chapter3
+
+import "fmt"
+
+func Run() {
+	fmt.Println("chapter3")
+}
+func DryRun() {
+}
--- system_programming/main.go
+++ system_programming/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/longtime1116/go_sandbox/system_programming/chapter2"
+	"github.com/longtime1116/go_sandbox/system_programming/chapter3"
 )
 
 func main() {
-	chapter2.Run()
+	chapter2.DryRun()
+	chapter3.Run()
 }
