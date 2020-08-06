4e86af4921484fc977f01fe142567e06fecfea88
shumo.tokinaga <longtime1116@gmail.com>
2020-08-06 19:45:01 +0900

    Q3.2

--- system_programming/chapter3/exec.go
+++ system_programming/chapter3/exec.go
@@ -1,24 +1,40 @@
 package chapter3
 
 import (
+	"crypto/rand"
 	"io"
 	"os"
 )
 
 func Run() {
+	if false {
+		{
+			// Q3.1
+			src, err := os.Open("./chapter3/q3_1_1.txt")
+			if err != nil {
+				panic(err)
+			}
+			defer src.Close()
+			dst, err := os.Create("./chapter3/q3_1_2.txt")
+			if err != nil {
+				panic(err)
+			}
+			defer dst.Close()
+			io.Copy(dst, src)
+		}
+	} // false end
 	{
-		// Q3.1
-		src, err := os.Open("./chapter3/q3_1.txt")
+		// Q3.2
+		f, err := os.Create("./chapter3/q3_2.txt")
 		if err != nil {
 			panic(err)
 		}
-		defer src.Close()
-		dst, err := os.Create("./chapter3/q3_2.txt")
+		buf := make([]byte, 1024)
+		_, err = rand.Read(buf)
 		if err != nil {
 			panic(err)
 		}
-		defer dst.Close()
-		io.Copy(dst, src)
+		f.WriteString(string(buf))
 	}
 }
 func DryRun() {
