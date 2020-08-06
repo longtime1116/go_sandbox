97896c20274f9038d352a60a340d51dd0938c9fd
shumo.tokinaga <longtime1116@gmail.com>
2020-08-06 23:12:41 +0900

    Q3.3

--- system_programming/chapter3/exec.go
+++ system_programming/chapter3/exec.go
@@ -1,9 +1,11 @@
 package chapter3
 
 import (
+	"archive/zip"
 	"crypto/rand"
 	"io"
 	"os"
+	"strings"
 )
 
 func Run() {
@@ -22,29 +24,52 @@ func Run() {
 			defer dst.Close()
 			io.Copy(dst, src)
 		}
+		{
+			// Q3.2
+			// 1
+			f, err := os.Create("./chapter3/q3_2_1")
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			buf := make([]byte, 1024)
+			_, err = rand.Read(buf)
+			if err != nil {
+				panic(err)
+			}
+			f.WriteString(string(buf))
+			// 2
+			f, err = os.Create("./chapter3/q3_2_2")
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			_, err = io.CopyN(f, rand.Reader, 1024)
+			if err != nil {
+				panic(err)
+			}
+		}
 	} // false end
 	{
-		// Q3.2
-		// 1
-		f, err := os.Create("./chapter3/q3_2_1")
+		// Q3.3
+		// 1. q3_3_dest.gz を unzip すると q3.txt が展開され、中にはHello, Worldが書かれている
+		f, err := os.Create("./chapter3/q3_3_dst.zip")
 		if err != nil {
 			panic(err)
 		}
-		buf := make([]byte, 1024)
-		_, err = rand.Read(buf)
+		defer f.Close()
+		zipWriter := zip.NewWriter(f)
+		defer zipWriter.Close()
+		w1, err := zipWriter.Create("./file1.txt")
 		if err != nil {
 			panic(err)
 		}
-		f.WriteString(string(buf))
-		// 2
-		f, err = os.Create("./chapter3/q3_2_2")
+		io.Copy(w1, strings.NewReader("Hello, world! This is file1.\n"))
+		w2, err := zipWriter.Create("./file2.txt")
 		if err != nil {
 			panic(err)
 		}
-		_, err = io.CopyN(f, rand.Reader, 1024)
-		if err != nil {
-			panic(err)
-		}
+		io.Copy(w2, strings.NewReader("Hello, world! This is file2.\n"))
 	}
 }
 func DryRun() {
