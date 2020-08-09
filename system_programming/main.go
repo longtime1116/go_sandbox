bfab77c339cd5e13440d9d43dcde842c8b901683
shumo.tokinaga <longtime1116@gmail.com>
2020-08-10 02:41:33 +0900

    Q4.1

--- system_programming/main.go
+++ system_programming/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/longtime1116/go_sandbox/system_programming/chapter2"
 	"github.com/longtime1116/go_sandbox/system_programming/chapter3"
+	"github.com/longtime1116/go_sandbox/system_programming/chapter4"
 )
 
 func main() {
 	chapter2.DryRun()
-	chapter3.Run()
+	chapter3.DryRun()
+	chapter4.Run()
 }
